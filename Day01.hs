2b428ea6a13fa264fd8578540ae5c9953a67fa8c
Damien Ragoucy <dam@ramdam.io>
2021-04-16 10:27:54 +0200

    Improve Haskell code for day 1

--- Day01.hs
+++ Day01.hs
@@ -15,20 +15,33 @@ transformInput s =
   & L.filter (null >>> not)
   & L.map read
 
+input' :: IO [Int]
 input' = do
   line <- SIO.readFile "data/day01.txt"
   return (line & transformInput)
 
-resultPart1 = do
-  input <- input'
+--- Part 1
+
+resultPart1 :: [Int] -> Int
+resultPart1 input =
   [ (x, y) | x <- input, y <- input, x + y == 2020 ]
     & L.head
     & uncurry (*)
-    & return
 
-resultPart2 = do
+getResultPart1 :: IO Int
+getResultPart1 = do
   input <- input'
+  return (resultPart1 input)
+
+--- Part 2
+
+resultPart2 :: [Int] -> Int
+resultPart2 input =
   [ (x, y, z) | x <- input, y <- input, z <- input, x + y + z == 2020 ]
     & L.head
     & \(x, y, z) -> x * y * z
-    & return
+
+getResultPart2 :: IO Int
+getResultPart2 = do
+  input <- input'
+  return (resultPart2 input)
